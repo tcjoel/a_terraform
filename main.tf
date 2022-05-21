9d24313e3a3c2207afca3eed7a9522944b640e01
tcjoel <tcjoel@gmail.com>
2022-05-21 17:23:26 -0400

    changing de repository name

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "s3_bucket" {
-  source = "git@github.com:tcjoel/a_terraform?ref=s3"
-  bucket_name = my_first_bucket_from_git_22
+  source      = "git@github.com:tcjoel/a_terraform?ref=s3"
+  bucket_name = "myfirst-bucket-fromgit-22"
 
 }
 
 /* module "ecr_repository" {
   source = "git@github.com:tcjoel/a_terraform?ref=ecr"
-  bucket_name = my_first_ecr_from_git_22
+  bucket_name = myfirst-ecr-fromgit-22
 
 } */
