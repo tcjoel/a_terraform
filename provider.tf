fe25ae5a07529e454b8998fcf699207cc957fa53
tcjoel <tcjoel@gmail.com>
2022-05-21 16:28:08 -0400

    removing a provider file

--- provider.tf
+++
@@ -1,7 +0,0 @@
-/* terraform {
-    backend "s3" {
-        bucket = "terraform-tomcat-bucket-joel-2022"
-        key = "logs/state"
-        region = "us-east-2"
-    }
-} */
--- variable.tf
+++ variable.tf
@@ -1,2 +1,3 @@
 variable "ecr_name" {
+    
 }
