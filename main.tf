7f7bd3f00e46d76213928562b9aaa4ec830fcd34
tcjoel <94588615+tcjoel@users.noreply.github.com>
2022-05-21 16:03:42 -0400

    adding tag to git ecr module

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 resource "aws_ecr_repository" "my_ecr" {
   name                 = var.ecr_name
   image_tag_mutability = "MUTABLE"
-
+  tags = {
+    Name        = "first_ecr_create_from_git"
+    Environment = "Dev"
+  }
   image_scanning_configuration {
     scan_on_push = false
   }
