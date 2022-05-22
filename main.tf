714232f2ccb0bd67bccdad2433e552560fcbf7e0
tcjoel <tcjoel@gmail.com>
2022-05-22 11:32:29 -0400

    fix the source code of our module

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "s3_bucket" {
-  source      = "git@github.com:tcjoel/a_terraform?ref=s3"
+  source      = "git::https://github.com/tcjoel/a_terraform.git?ref=s3"
   bucket_name = "myfirst-bucket-fromgit-22"
 
 }
 
 /* module "ecr_repository" {
-  source = "git@github.com:tcjoel/a_terraform?ref=ecr"
+  source = "git::https://github.com/tcjoel/a_terraform.git?ref=ecr"
   ecr_name = "myfirst-ecr-fromgit-22"
 
 } */
